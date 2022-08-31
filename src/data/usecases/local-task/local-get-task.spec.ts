4563c9cb2c0990f8c704bf63dbfa4e3925a8877f
Peterson Pereira Bozza <petersonbozza7@gmail.com>
2022-08-31 10:08:05 -0300

    move instances to a factory method

--- src/data/usecases/local-task/local-get-task.spec.ts
+++ src/data/usecases/local-task/local-get-task.spec.ts
@@ -1,11 +1,20 @@
 import { GetTaskStoreSpy } from "@/data/tests/get-task-mock";
 import { LocalGetTask } from "@/data/usecases/local-task/local-get-task";
 
+const makeSut = () => {
+  const getTaskStoreSpy = new GetTaskStoreSpy();
+  const localGetTask = new LocalGetTask(getTaskStoreSpy);
+
+  return {
+    sut: localGetTask,
+    getTaskStoreSpy,
+  };
+};
+
 describe("LocalGetTask", () => {
   it("should not call getAll if get is called", async () => {
-    const getTaskStoreSpy = new GetTaskStoreSpy();
-    const localGetTask = new LocalGetTask(getTaskStoreSpy);
-    await localGetTask.get("any_id");
+    const { getTaskStoreSpy, sut } = makeSut();
+    await sut.get("any_id");
     expect(getTaskStoreSpy.fetchItemCount).toBe(1);
     expect(getTaskStoreSpy.fetchAllCount).toBe(0);
   });
