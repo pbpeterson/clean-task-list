32a42ee06f8d1c3fd46ebbad3ed350dcafee8a9b
Peterson Pereira Bozza <petersonbozza7@gmail.com>
2022-09-01 15:54:06 -0300

    add home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,21 @@
-function HomePage() {
-  return <div>Welcome to Next.js!</div>;
-}
+import { makeDeleteTask } from "@/presentation/factories/local-delete-task.factory";
+import { makeGetTask } from "@/presentation/factories/local-get-task-factory";
+import { makeSaveTask } from "@/presentation/factories/local-save-task-factory";
+import HomeTemplate from "@/presentation/Home/index";
+import { GlobaStyles } from "@/presentation/styles/global";
 
-export default HomePage;
+const Home = () => {
+  GlobaStyles();
+
+  return (
+    <div>
+      <HomeTemplate
+        localDeleteTask={makeDeleteTask()}
+        localGetTask={makeGetTask()}
+        localSaveTask={makeSaveTask()}
+      />
+    </div>
+  );
+};
+
+export default Home;
