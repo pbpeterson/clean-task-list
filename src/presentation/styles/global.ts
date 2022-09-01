e9a2ce580c324a781ccba921fa79618ce557ab7c
Peterson Pereira Bozza <petersonbozza7@gmail.com>
2022-09-01 15:50:29 -0300

    add global style

---
+++ src/presentation/styles/global.ts
@@ -0,0 +1,15 @@
+import { globalCss } from "@stitches/react";
+
+export const GlobaStyles = globalCss({
+  "*": {
+    boxSizing: "border-box",
+    margin: "0",
+    padding: 0,
+  },
+
+  body: {
+    fontSize: "62.5%",
+    fontFamily: "sans-serif",
+    background: "#3098e1",
+  },
+});
