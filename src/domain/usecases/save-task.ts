4cb5efd9ac864747ac8647d33951972b2ff4bdba
Peterson Pereira Bozza <petersonbozza7@gmail.com>
2022-09-01 11:50:24 -0300

    fix return of SaveTask domain usecase

--- src/domain/usecases/save-task.ts
+++ src/domain/usecases/save-task.ts
@@ -1,3 +1,9 @@
-interface SaveTask {
-  save: (id: number) => Promise<boolean>;
+import { TaskParams } from "../models/task-model";
+
+export interface SaveTask {
+  save: ({ content, id }: SaveTask.Params) => Promise<void>;
+}
+
+export namespace SaveTask {
+  export type Params = TaskParams;
 }
