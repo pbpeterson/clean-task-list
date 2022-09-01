a58a8ed8d9f7125b2c4043794fd684a04433f98f
Peterson Pereira Bozza <petersonbozza7@gmail.com>
2022-09-01 16:37:15 -0300

    add favicon and title

---
+++ src/pages/_document.tsx
@@ -0,0 +1,16 @@
+import { Html, Head, Main, NextScript } from "next/document";
+
+export default function Document() {
+  return (
+    <Html>
+      <Head>
+        <title>Pb - TaskList</title>
+        <link rel="shortcut icon" href="favicon.png" type="image/png" />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
