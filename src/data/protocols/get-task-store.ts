0353612c52fb012f2e28e076f043842607af0071
Peterson Pereira Bozza <petersonbozza7@gmail.com>
2022-09-01 10:41:29 -0300

    add new method to protocol get task store

--- src/data/protocols/get-task-store.ts
+++ src/data/protocols/get-task-store.ts
@@ -4,4 +4,6 @@ export interface GetTaskStore {
   fetchItem: (key: string, id: number) => TaskParams | null;
 
   fetchAll: (key: string) => Array<TaskParams> | [];
+
+  clearAllTasks: (key: string) => Array<TaskParams>;
 }
