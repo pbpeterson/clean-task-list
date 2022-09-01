8bc20718284bf6e981131ae00a35e2ff3ba28ca1
Peterson Pereira Bozza <petersonbozza7@gmail.com>
2022-09-01 10:43:16 -0300

    ensure GetTaskStoreSpy implements RemoveTaskStore protocol

--- src/data/tests/get-task-mock.ts
+++ src/data/tests/get-task-mock.ts
@@ -1,7 +1,8 @@
 import { TaskParams } from "@/domain/models/task-model";
 import { GetTaskStore } from "@/data/protocols/get-task-store";
+import { RemoveTaskStore } from "../protocols/remove-task-store";
 
-export class GetTaskStoreSpy implements GetTaskStore {
+export class GetTaskStoreSpy implements GetTaskStore, RemoveTaskStore {
   allTasks: Array<TaskParams> = [];
   fetchAllCount = 0;
   fetchItemCount = 0;
@@ -15,6 +16,11 @@ export class GetTaskStoreSpy implements GetTaskStore {
 
   fetchAll(key: string) {
     this.fetchAllCount++;
+    return this.allTasks;
+  }
+
+  clearAllTasks(key: string): Array<TaskParams> {
+    this.allTasks = [];
     return this.allTasks;
   }
 }
