9e15f9fb16511b76aeb4ef1a343deaf5b359938e
Peterson Pereira Bozza <petersonbozza7@gmail.com>
2022-09-01 10:43:41 -0300

    ensure LocalRemoveTask will remove all when clear all is called

---
+++ src/data/usecases/local-task/local-remove-task.spec.ts
@@ -0,0 +1,38 @@
+import { RemoveTaskStore } from "@/data/protocols/remove-task-store";
+import { GetTaskStoreSpy } from "@/data/tests/get-task-mock";
+import { makeTasksList } from "@/data/tests/tasks";
+import { TaskParams } from "@/domain/models/task-model";
+import { LocalGetTask } from "./local-get-task";
+
+class LocalRemoveTask {
+  constructor(private readonly remoteTaskStore: RemoveTaskStore) {}
+  async removeAll(key: string): Promise<Array<TaskParams>> {
+    const tasks = this.remoteTaskStore.clearAllTasks(key);
+    return tasks;
+  }
+}
+
+describe("LocalRemoveTask", () => {
+  const makeSut = () => {
+    const getTaskStoreSpy = new GetTaskStoreSpy();
+    const localRemoveTask = new LocalRemoveTask(getTaskStoreSpy);
+    const localGetTask = new LocalGetTask(getTaskStoreSpy);
+
+    return {
+      sut: localRemoveTask,
+      getTaskStoreSpy,
+      localGetTask,
+    };
+  };
+
+  it("should clear all tasks when clearAll is called", async () => {
+    const { getTaskStoreSpy, sut, localGetTask } = makeSut();
+    const tasksMock = makeTasksList();
+    getTaskStoreSpy.allTasks = tasksMock;
+    let tasks = await localGetTask.getAll("any_key");
+    expect(tasks).toEqual(tasksMock);
+    await sut.removeAll("any_key");
+    tasks = await localGetTask.getAll("any_key");
+    expect(tasks).toEqual([]);
+  });
+});
