5ffffc75e46bf63b4dc9f1c5f0e2ab2a5f2160fd
Peterson Pereira Bozza <petersonbozza7@gmail.com>
2022-09-01 11:22:38 -0300

    remove unnecessary import

--- src/data/protocols/remove-task-store.ts
+++ src/data/protocols/remove-task-store.ts
@@ -1,5 +1,3 @@
-import { TaskParams } from "@/domain/models/task-model";
-
 export interface RemoveTaskStore {
   clearAllTasks: (key: string) => void;
   clearById: (key: string, id: number) => void;
