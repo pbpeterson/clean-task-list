1d7a09b3562ab9ec108b9c53b0ade545b16d7b8d
Peterson Pereira Bozza <petersonbozza7@gmail.com>
2022-09-01 14:38:41 -0300

    move LocalStorageDoActions to its own file

--- src/infra/usecases/localstorage-save-task.spec.ts
+++ src/infra/usecases/localstorage-save-task.spec.ts
@@ -1,55 +1,9 @@
-import { GetTaskStore } from "@/data/protocols/get-task-store";
-import { RemoveTaskStore } from "@/data/protocols/remove-task-store";
-import { SaveTaskStore } from "@/data/protocols/save-task-store";
 import { makeTasksList } from "@/data/tests/tasks";
 import { LocalAddTask } from "@/data/usecases/local-task/local-add-task";
 import { LocalGetTask } from "@/data/usecases/local-task/local-get-task";
 import { LocalRemoveTask } from "@/data/usecases/local-task/local-remove-task";
-import { TaskParams } from "@/domain/models/task-model";
 import "jest-localstorage-mock";
-
-class LocalStorageDoActions
-  implements SaveTaskStore, GetTaskStore, RemoveTaskStore
-{
-  addTask({ content, id }: TaskParams) {
-    const getOldValues = JSON.parse(localStorage.getItem("pbTaskList"));
-    let values = [];
-    if (getOldValues === null) {
-      values.push({ content, id });
-      localStorage.setItem("pbTaskList", JSON.stringify(values));
-      return;
-    }
-    values.push(...getOldValues, { content, id });
-    localStorage.setItem("pbTaskList", JSON.stringify(values));
-  }
-
-  fetchAll(key: string): Array<TaskParams> {
-    const items = JSON.parse(localStorage.getItem(key));
-    return items;
-  }
-
-  fetchItem(key: string, id: number): TaskParams {
-    const items = this.fetchAll(key);
-    const currentItem = items.find((task) => task.id === id);
-    return currentItem;
-  }
-
-  clearAllTasks(key: string): void {
-    localStorage.setItem(key, JSON.stringify([]));
-  }
-
-  clearById(key: string, id: number) {
-    const getOldValues: Array<TaskParams> = JSON.parse(
-      localStorage.getItem("pbTaskList")
-    );
-    let newValues = [];
-    if (getOldValues === null) {
-      return;
-    }
-    newValues = getOldValues.filter((task) => task.id !== id);
-    localStorage.setItem(key, JSON.stringify(newValues));
-  }
-}
+import { LocalStorageDoActions } from "./localstorage.save.task";
 
 const makeSut = () => {
   const sut = new LocalStorageDoActions();
---
+++ src/infra/usecases/localstorage.save.task.ts
@@ -0,0 +1,47 @@
+import { GetTaskStore } from "@/data/protocols/get-task-store";
+import { RemoveTaskStore } from "@/data/protocols/remove-task-store";
+import { SaveTaskStore } from "@/data/protocols/save-task-store";
+import { TaskParams } from "@/domain/models/task-model";
+
+export class LocalStorageDoActions
+  implements SaveTaskStore, GetTaskStore, RemoveTaskStore
+{
+  addTask({ content, id }: TaskParams) {
+    const getOldValues = JSON.parse(localStorage.getItem("pbTaskList"));
+    let values = [];
+    if (getOldValues === null) {
+      values.push({ content, id });
+      localStorage.setItem("pbTaskList", JSON.stringify(values));
+      return;
+    }
+    values.push(...getOldValues, { content, id });
+    localStorage.setItem("pbTaskList", JSON.stringify(values));
+  }
+
+  fetchAll(key: string): Array<TaskParams> {
+    const items = JSON.parse(localStorage.getItem(key));
+    return items;
+  }
+
+  fetchItem(key: string, id: number): TaskParams {
+    const items = this.fetchAll(key);
+    const currentItem = items.find((task) => task.id === id);
+    return currentItem;
+  }
+
+  clearAllTasks(key: string): void {
+    localStorage.setItem(key, JSON.stringify([]));
+  }
+
+  clearById(key: string, id: number) {
+    const getOldValues: Array<TaskParams> = JSON.parse(
+      localStorage.getItem("pbTaskList")
+    );
+    let newValues = [];
+    if (getOldValues === null) {
+      return;
+    }
+    newValues = getOldValues.filter((task) => task.id !== id);
+    localStorage.setItem(key, JSON.stringify(newValues));
+  }
+}
